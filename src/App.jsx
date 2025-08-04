059d510803aa4e2a135c4a182bbf847a37341301
Matias Casiba <casibagabriel@gmail.com>
2025-08-04 11:10:48 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Route } from 'react-router'
+import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
